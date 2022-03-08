23222750af430f85bbdf7f78ad9c489b424fcd98
Brandon <brandonhernandez_10@outlook.es>
2022-03-07 18:21:50 -0600

    init lab

---
+++ SudokuValidator.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int check_row() {
+
+}
+
+int check_column() {
+
+}
+
+int check_groupp() {
+
+}
+
+int main(int argc, char** argv) {
+  char* sudoku_path = argv[1];
+  
+
+  return 0;
+}
